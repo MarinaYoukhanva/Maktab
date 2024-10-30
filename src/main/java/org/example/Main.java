051f7e273fd921d3ef91e525fd689610b8cb0bc5
MarinaYoukhanva <166758761+MarinaYoukhanva@users.noreply.github.com>
2024-10-31 01:53:53 +0330

    completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,40 @@ import org.example.entity.Rank;
 import org.example.entity.Soldier;
 import org.example.serivce.SoldierService;
 
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
         SoldierService soldierService = new SoldierService();
+        int corporalCounter = 0;
+        int roundCounter = 0;
+
         soldierService.soldierInit();
 
-        for (int i = 0; i < Database.getAllSoldiers().length; i++){
-            System.out.println(Database.getAllSoldiers()[i]);
+        for (int i = 0; i < Database.getAllSoldiers().length; i++) {
+            Soldier soldier = Database.getAllSoldiers()[i];
+            System.out.println(soldier);
         }
+        System.out.println("Soldiers initialized successfully \n");
+
+
+        do {
+            for (int i = 0; i < Database.getAllSoldiers().length; i++) {
+                Soldier soldier = Database.getAllSoldiers()[i];
+                if (soldierService.isPassed(soldier))
+                    soldierService.promoteRank(soldier);
+                else
+                    soldierService.demoteRank(soldier);
+
+                System.out.println(soldier);
+                if (soldier.getRank() == Rank.Corporal)
+                    corporalCounter++;
+
+            }
+            roundCounter++;
+            System.out.println("number of corporals: " + corporalCounter);
+            System.out.println("number of rounds: " + roundCounter + "\n");
+        } while (corporalCounter > 9);
 
     }
 }
--- src/main/java/org/example/serivce/SoldierService.java
+++ src/main/java/org/example/serivce/SoldierService.java
@@ -1,8 +1,7 @@
 package org.example.serivce;
 
 import org.example.database.Database;
-import org.example.entity.Rank;
-import org.example.entity.Soldier;
+import org.example.entity.*;
 
 import java.util.Random;
 
@@ -10,21 +9,37 @@ import java.util.Random;
 public class SoldierService {
 
     Random rand = new Random();
-    Database db ;
+    Database db;
 
-    public boolean isPassed (Soldier soldier){
+    public boolean isPassed(Soldier soldier) {
         int finalScore = 0;
-        for (int i = 0; i < soldier.getShootings().length; i++){
+        for (int i = 0; i < soldier.getShootings().length; i++) {
             finalScore += soldier.getShootings()[i];
+            soldier.setFinalScore(finalScore);
         }
         return finalScore >= 28;
     }
-    public Rank setRandomRank (){
+
+    public void promoteRank(Soldier soldier) {
+        Rank rank = soldier.getRank();
+        if (rank != Rank.Corporal){
+            soldier.setRank(Rank.values()[rank.ordinal() + 1]);
+        }
+    }
+    public void demoteRank(Soldier soldier) {
+        Rank rank = soldier.getRank();
+        if (rank != Rank.Lieutenant){
+            soldier.setRank(Rank.values()[rank.ordinal() - 1]);
+        }
+    }
+
+    public Rank setRandomRank() {
         int randomRank = rand.nextInt(3);
         return Rank.values()[randomRank];
     }
-    public void soldierInit(){
-        for (int i = 0; i < Database.getAllSoldiers().length; i++){
+
+    public void soldierInit() {
+        for (int i = 0; i < Database.getAllSoldiers().length; i++) {
             Soldier soldier = new Soldier();
             soldier.setRank(setRandomRank());
             soldier.setShootings(soldierShootScores());
@@ -32,12 +47,14 @@ public class SoldierService {
             Database.getAllSoldiers()[i] = soldier;
         }
     }
-    public int[] soldierShootScores (){
+
+    public int[] soldierShootScores() {
         int[] scores = new int[4];
-        for (int i = 0 ; i < 4; i++){
-            int randomScore = rand.nextInt(10);
-            scores[i] = randomScore + 1;
+        for (int i = 0; i < 4; i++) {
+            int randomScore = rand.nextInt(11);
+            scores[i] = randomScore;
         }
         return scores;
     }
+
 }
